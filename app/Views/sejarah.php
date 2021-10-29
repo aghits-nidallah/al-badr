bfde3371618b6bedebc330294718bcc9aae68403
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-29 23:46:29 +0700

    chore: sejarah

--- app/Views/sejarah.php
+++ app/Views/sejarah.php
@@ -13,4 +13,107 @@
         <h2 class="page-titlee">Al Badr</h2>
     </div>
 </section><!--pager-section end-->
+
+<section class="history-section">
+    <div class="container py-5">
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>1990</span></h2>
+            <p class="mb-4">Pendirian Yayasan Sunan Gunungjati (YSGJ) melalui Notaris Ili Rohaeli, SH dengan akte notaries Nomor 28 dengan pengurus untuk pertama kalinya:</p>
+            <div class="row">
+                <div class="col"></div>
+                <div class="col">
+                    <table class="table border">
+                        <tr>
+                            <td>Ketua</td>
+                            <td>:</td>
+                            <td>Tamar</td>
+                        </tr>
+                        <tr>
+                            <td>Wakil Ketua</td>
+                            <td>:</td>
+                            <td>Mudiya</td>
+                        </tr>
+                        <tr>
+                            <td>Sekretaris</td>
+                            <td>:</td>
+                            <td>Abdul Ghofar</td>
+                        </tr>
+                        <tr>
+                            <td>Bendahara</td>
+                            <td>:</td>
+                            <td>Karwiyat</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col"></div>
+            </div>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>1997</span></h2>
+            <p class="mb-4">Keraton Kasepuhan menerbitkan Ijin penggunaan tanah keraton untuk pendirian bangunan RATB melaui surat nomor 09/SS/SKR/KSP/97 tanggal 9 April 1997 yang ditandatangani oleh PRA Maulana Pakuningrat, SH dan para saksi baik dari keluarga keratin, pinisepuh dan pengurus Yayasan Sunan Gunungjati. Selanjutnya proses pembangunan dibimbing langsung dibawah pengawasan Sultan Jalaluddin dari Keraton Kanoman dan menandatangani prasasti pendirian gedung RATB saat peresmian.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>1998</span></h2>
+            <p class="mb-4">Pendirian RA Thola’al Badr melalui SK Yayasan SGJ Nomor 01/SKPRA/YSGJ/II/1998 dengan jumlah siswa 99 anak, 9 guru yang dikepalai oleh Ibu <b>Kholifah</b>.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>1999</span></h2>
+            <p class="mb-4">Terbit Rekomendasi Pendirian RATB dari Kantor Departemen Agama Kabupaten Cirebon Nomor: M.i-21/5-a/PP.00.4/564/1999.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>1999</span></h2>
+            <p class="mb-4">Mendapatkan Piagam Pendirian RA dari Kandepag Kabupaten Cirebon nomor: M.i-21/5-a/KP.004/13/2000 tanggal 27 Juni 2000 berdasarkan SK Kakandepag Kabupaten Cirebon nomor: D/Mi-21/RA/014/2000 dengan Nomor Statistik Sekolah (NSM) 012321115036.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2000</span></h2>
+            <p class="mb-4">Oleh karena Ibu <b>Kholifah</b> dipercaya memimpin RA Raudhatul Muntaha kota Cirebon, jabatan Kepala RA Thola’al Badr diserahkan kepada Ibu <b>Mu’minah, S.Ag</b>.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2007</span></h2>
+            <p class="mb-4">RATB mendapatkan pengakuan <b>Akreditasi</b> dengan nilai <b>A</b> melalui Keputusan Kakandepag Kab. Cirebon nomor: A/Kd.10.09/RA/016/2007 tanggal 23 Januari 2007.</p>
+            <p class="mb-4">Kandepag mengeluarkan perubahan NSRA RATB menjadi 012321115131 melalui Piagam Pendirian RA No. D/Kd.10.9/RA/110/2007 tanggal 16 Februari 2007 berdasarkan SK Kakandepag kab. Cirebon nomor: Kd/10.9/4/PP.004/1787/2007.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2008</span></h2>
+            <p class="mb-4">Ibu Mimin Mu’minah pindah domisili mengikuti suaminya yang studi ke Australia, maka jabatan Kepala RA Thola’al Badr dipegang oleh <b>Bp. Abdul Ghofar, S.Pd.I</b>.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2010</span></h2>
+            <p class="mb-4">Piagam Pendirian PAUD/RA nomor: 32-10.09/RA/4/PP.00.4/228/2010 merupakan Piagam pembaruan kedua yang menerbitkan NSM RATB menjadi 101232090089.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2013</span></h2>
+            <p class="mb-4">RATB menjadi RA Unggulan Kabupaten Cirebon dan sebagai RA Pembina di Kecamatan Gunungjati.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2016</span></h2>
+            <p class="mb-4">Pemutihan Yayasan Sunan Gunungjati menjadi Yayasan Pendidikan Sunan Gunung Jati melalui Akte Notaris No.09 Amin Mubarok, S.H.,M.Kn tanggal 07 Maret 2016 yang disahkan oleh Menteri Hukum dan HAM RI melalui SK Nomor AHU-0013420.AH.01.04. Tahun 2016.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2017</span></h2>
+            <p class="mb-4">Merintis pengadaan ruang kelas baru, saat jumlah siswa mencapai 160 siswa yang terbagi menjadi 6 rombel dengan 14 guru.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2018</span></h2>
+            <p class="mb-4">Rencana penambahan 3 ruang kelas baru terealisasi, sehingga jumlah ruang kelas seluruhnya menjadi 6 kelas untuk belajar 170 siswa dengan 16 orang guru.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2019</span></h2>
+            <p class="mb-4">Diakreditasi oleh Badan Akreditasi Nasional dengan nilai “A”  Nomor Piagam : 126/BAN PAUD DAN PNF/AKR/2019 Tanggal 07 Agustus 2019.</p>
+        </div>
+    </div>
+</section>
 <?= $this->endSection('content') ?>

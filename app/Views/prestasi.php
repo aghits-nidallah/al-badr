24a4ba895372e92595edeb6fed013cb7b7661f1f
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-29 23:58:46 +0700

    chore: prestasi

--- app/Views/prestasi.php
+++ app/Views/prestasi.php
@@ -13,4 +13,23 @@
         <h2 class="page-titlee">Al Badr</h2>
     </div>
 </section><!--pager-section end-->
+
+<section class="achievment-section py-5">
+    <div class="container">
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2013</span></h2>
+            <p class="mb-4">RA Thola’al Badr adalah RA Unggulan Kabupaten Cirebon dan hingga sekarang menjadi RA Unggulan di Kecamatan Gunungjati</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tahun <span>2017</span></h2>
+            <p class="mb-4">Juara I Karya Tulis Ke-RA-an tingkat Kabupaten Cirebon.</p>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Lainnya</h2>
+            <p class="mb-4">Dalam berbagai momen lomba baik antar sekolah maupun event peringatan, Santri RA Thola’al Badr selalu mendapat tempat Juara baik di Tingkat Kecamatan maupun Kabupaten,</p>
+        </div>
+    </div>
+</section>
 <?= $this->endSection('content') ?>

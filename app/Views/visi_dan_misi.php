f370141983a98f78f9c1a5965283565016c01b15
NikarashiHatsu <yourlovelydev@gmail.com>
2021-10-29 23:56:15 +0700

    chore: visi & misi

--- app/Views/visi_dan_misi.php
+++ app/Views/visi_dan_misi.php
@@ -13,4 +13,74 @@
         <h2 class="page-titlee">Al Badr</h2>
     </div>
 </section><!--pager-section end-->
+
+<section class="history-section">
+    <div class="container py-5">
+        <div class="section-title text-center mb-5">
+            <h2>Visi</h2>
+            <p class="mb-4">"Menjadi Lembaga  Cerdas Mencetak Lulusan yang CerDAS”</p>
+            <p class="mb-4">Lembaga yang cerdas adalah lembaga yang mampu mengkelola potensinya untuk mencapai hasil pembelajaran maksimal demi terwujudnya tujuan pendidikan nasional.</p>
+            <p class="mb-4">Sedangkan Lulusan yang CerDAS adalah lulusan yang:</p>
+            <div class="row">
+                <div class="col-2"></div>
+                <div class="col">
+                    <table class="table border text-left">
+                        <tr>
+                            <td>CERIA</td>
+                            <td>:</td>
+                            <td>Adalah ciri optimsme dalam menatap,menongsong dan menghadapi masa depan dengan segala masalah dan tantangannya.</td>
+                        </tr>
+                        <tr>
+                            <td>BERBUDAYA</td>
+                            <td>:</td>
+                            <td>Mengenali, memahami dan melestarikan nilai-nilai positif budaya dan kearifan lokal sebagai titis waris kebijakan dan hasil daya, karya, cipta para sepuh dalam rangka memperkenakan Islam kepada lingkungannya.</td>
+                        </tr>
+                        <tr>
+                            <td>AGAMIS</td>
+                            <td>:</td>
+                            <td>Mengenali, memahami dan mengamalkan nlai-nilai Ajara Islam sebagai Way of Life setiap muslim.</td>
+                        </tr>
+                        <tr>
+                            <td>SEIMBANG</td>
+                            <td>:</td>
+                            <td>Terbentuknya pribadi yang seimbang dalam segala sisi sebagai wujud terapan hukum alam. Seimbang prinsip kehidupan duniawi dan ukhrowi, pengembangan otak kiri dan kanan, perkembangan jiwa dan raga serta seimbang dalam penguasaan dasar-dasar IPTEK dan IMTAKnya.</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-2"></div>
+            </div>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2><span>Misi</span></h2>
+            <div class="text-left">
+                <p class="mb-3">A. Mencetak lulusan yang mampu membaca sesuai tugas perkembangannya.</p>
+                <p class="mb-3">B. Mencetak lulusan yang memahami nilai-nilai positif kearifan lokal serta apresiatif terhadap tradisi yang berlaku sebagai produk kebudayaan daerahnya berdasarkan nilai-nilai agama.</p>
+                <p class="mb-3">C. Mencetak lulusan yang ceria, mandiri, kompetititf, berprestasi.</p>
+                <p class="mb-3">D. Mencetak lulusan yang sehat dan tumbuh kembang yang seimbang.</p>
+                <p class="mb-3">E. Meningkatkan profesionalisme pada pengelolaan administrasi dan anggaran sekolah untuk mempertahankan predikat sebagai RA Unggulan di Kecamatan Gunung Jati dan Kabupaten Cirebon.</p>
+                <p class="mb-3">F. Menjalin kemitraan kerjasama dengan orang tua murid dan instansi yang terkait dalam rangka pengembangan potensi RA Thola’al Badr.</p>
+                <p class="mb-3">G. Optimalisasi Tata tertib sekolah</p>
+                <p class="mb-3">H. Menciptakan suasana sekolah berwawasan lingkungan yang agamis dan melestarikan budaya untuk menjadi sentra kegiatan dan laboratorium PAUD di Gunung Jati</p>
+            </div>
+        </div>
+
+        <div class="section-title text-center mb-5">
+            <h2>Tujuan</h2>
+            <div class="text-left">
+                <p class="mb-3">A. Membangun landasan bagi berkembangnya peserta didik agar menjadi insan berahlak, sehat, berilmu, cakap, kreatif. Inovatif, mandiri dan percata diri</p>
+                <p class="mb-3">B. Mengembangkan potensi kecerdasan spiritual, intelectual, emosiaonal dan social peserta didik pada masa pertumbuhan dalam lingkungan bermain yang edukatif dan menyenangkandan bisa diukur yaitu :</p>
+                <p class="mb-3">C. Terwujudnya lingkungan dengan anak-anak yang mampu melayani diri sendiri sesuai lingkungannya</p>
+                <p class="mb-3">D. Berkembangnya potensi berkreasi peserta didik sehingga dapat menyesuaikan diri dengan lingkungan yang menyenangkan </p>
+                <p class="mb-3">E. Terwujudnya lingkungan dengan suasana anak-anak ceria yang mencintai pengetahuan</p>
+                <p class="mb-3">F. Terbentuknya pribadi-pribadi yang mampu menolong diri sendiri</p>
+                <p class="mb-3">G. Mengajarkan hafalan surat-surat pendek dan do’a harian</p>
+                <p class="mb-3">H. Mengajarkan membaca huruf hijaiyyah, huruf latin</p>
+                <p class="mb-3">I. Mengenalkan angka, bilangan, warna, bangun</p>
+                <p class="mb-3">J. Mengenalkan ritus Ibadah</p>
+                <p class="mb-3">K. Mengenalkan, melatih dan membiasakan tatakrama dasar pergaulan antar sesama, dengan guru dan orangtua mereka dalam lingkungannya.</p>
+            </div>
+        </div>
+    </div>
+</section>
 <?= $this->endSection('content') ?>
